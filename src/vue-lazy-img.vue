35efcf6eb0ac42f5b479dceccdf40f095ac4a67a
lakb248 <lakb248@gmail.com>
2016-09-22 14:33:29 +0800

    complete the first version of vue-lazy-img

--- src/vue-lazy-img.vue
+++ src/vue-lazy-img.vue
@@ -1,15 +1,73 @@
 <template>
-    <h2>{{text}}</h2>
+    <img class="v-lazy-image" v-bind:src="placeholder"/>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                text: 'Hello Vue-Component'
-            };
-        }
+
+// whether the scroll event listener has been added
+var hasListener = false;
+
+/**
+ * load image async
+ * @param  {Object} element the img element
+ * @param  {String} source  the source of the image
+ */
+var loadImage = function (element, source) {
+    var img = new Image();
+    img.onload = () => {
+        // set the src of the element
+        // and set the loaded flag to 1
+        element.src = source;
+        element.setAttribute('data-loaded', '1');
     };
+    img.src = source;
+};
+
+/**
+ * load image that in the screen
+ * @param  {Object} elements the img elements
+ * @param  {String} source   the source of the image
+ */
+var loadImagesIfNeed = function (elements, source) {
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    var length = elements.length;
+    for(var i = 0; i < length; i ++) {
+        var element = elements[i];
+        // if the source of the element has been loaded,
+        // skip the element
+        if (+element.getAttribute('data-loaded') === 1) {
+            continue;
+        }
+        var height = element.offsetHeight;
+        var position = element.getBoundingClientRect();
+        // whether the element is in the screen
+        if (position.top > 0 && position.top + height < windowHeight) {
+            loadImage(element, source);
+        }
+    }
+};
+export default {
+    props: ['source', 'placeholder'],
+    ready() {
+        // only add one listener to the scroll event
+        if (!hasListener) {
+            hasListener = true;
+            var timeout = -1;
+            window.addEventListener('scroll', () => {
+                // function throttle
+                if (timeout !== -1) {
+                    clearTimeout(timeout);
+                }
+                // get element in the screen and load images
+                timeout = setTimeout(() => {
+                    var elements = document.querySelectorAll('.v-lazy-image');
+                    loadImagesIfNeed(elements, this.source);
+                }, 300);
+            });
+        }
+        // load image oninit
+        loadImagesIfNeed([this.$el], this.source);
+    }
+};
 </script>
 
-<style lang="sass"></style>
